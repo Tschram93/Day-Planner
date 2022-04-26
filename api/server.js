cc91594ec19bc02f5a0f225078975f66c4758258
TS93 <tschram93@yahoo.com>
2022-04-26 19:45:21 -0400

    connecting server to mongooseDB and running server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,19 @@
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
+const PORT = 3001;
 
 const app =  express();
 
 app.use(express.json());
 // use cors to prevent any cross-origin errors
 app.use(cors());
+
+mongoose.connect('mongodb://127.0.0.1:27017/day-planner', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('Connected to DataBase'))
+    .catch(console.error);
+
+app.listen(PORT, () => console.log('Server running on port: 3001'));
