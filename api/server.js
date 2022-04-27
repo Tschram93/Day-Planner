2d3c072c0a59988e1167d2d5da0d8ba3abd884c6
TS93 <tschram93@yahoo.com>
2022-04-27 18:23:57 -0400

    added config and connections for backend

--- api/server.js
+++ api/server.js
@@ -1,53 +1,63 @@
 const cors = require('cors');
 const DayPlanner = require('./models/dayPlanner');
+const db = require('./config/connection');
 const express = require('express');
 const mongoose = require('mongoose');
 
-const app =  express();
+const app = express();
+const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // use cors to prevent any cross-origin errors
 app.use(cors());
 
-mongoose.connect('mongodb://127.0.0.1:27017/day-planner', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log('Connected to DataBase'))
-    .catch(console.error);
 
-    // Get request
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+// Get request
 app.get('/todos', async (req, res) => {
-    const todos = await DayPlanner.find();
+	const todos = await DayPlanner.find();
 
-    res.json(todos);
+	res.json(todos);
 });
 
-    // POST request
+// POST request
 app.post('/todo/new', (req, res) => {
-    const todo = new DayPlanner({
-        text: req.body.text
-    });
-    todo.save()
+	const todo = new DayPlanner({
+		text: req.body.text,
+	});
+	todo.save();
 
-    res.json(todo);
+	res.json(todo);
 });
 
-    // Delete Request
+// Delete Request
 app.delete('/todo/delete/:id', async (req, res) => {
-    const result = await DayPlanner.findByIdAndDelete(req.params.id);
+	const result = await DayPlanner.findByIdAndDelete(req.params.id);
 
-    res.json(result);
-})
+	res.json(result);
+});
 
-    // PUT
+// PUT
 app.get('/todo/complete/:id', async (req, res) => {
-    const todo = await DayPlanner.findById(req.params.id);
+	const todo = await DayPlanner.findById(req.params.id);
 
-    todo.complete = !todo.complete;
-    todo.save();
+	todo.complete = !todo.complete;
+	todo.save();
 
-    res.json(todo);
-})
+	res.json(todo);
+});
 
-app.listen(3001, () => console.log('Server running on port: 3001'));
+db.once('open', () => {
+app.listen(PORT, () => {
+	console.log(`Server running on http://localhost:${PORT}!`)
+    });
+});
