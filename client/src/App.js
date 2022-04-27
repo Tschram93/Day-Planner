5c1e9ed7f01a171c827f828633d9131b8124e6e3
TS93 <tschram93@yahoo.com>
2022-04-26 22:02:35 -0400

    fetch DB

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
 import { useState, useEffect } from 'react';
 
+const API_BASE = 'http://localhost:3001';
+
 function App() {
   const [tasks, setTasks] = useState([]);
   const [popupActive, setPopupActive] = useState(false);
+  const [newTask, setNewTask] = useState('');
+
+  useEffect(() => {
+    GetTasks();
+  }, [])
+
+  const GetTasks = () => {
+    fetch(API_BASE + '/todos')
+      .then(res => res.json())
+      .then(data => setTasks(data))
+      .catch(err => console.log(err));
+  }
+
 	let name = 'Travis';
 	return (
 		<div className='App'>
