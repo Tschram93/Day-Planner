3b5f27b41aba8983fe4509f35da97f234b15ef93
TS93 <tschram93@yahoo.com>
2022-04-26 21:09:13 -0400

    hard coding elements for styling development

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	let name = 'Travis';
+	return (
+		<div className='App'>
+			<h1>Welcome, {name}</h1>
+			<h4>Your Tasks</h4>
+			<div className='tasks'>
+				<div className='task'>
+					<div className='checkbox'></div>
+					<div className='text'>Make Breakfast</div>
+          <div className="delete-task">X</div>
+				</div>
+        <div className="task is-complete">
+          <div className="checkbox"></div>
+          <div className="text">Get the eggs</div>
+          <div className="delete-task">X</div>
+        </div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
